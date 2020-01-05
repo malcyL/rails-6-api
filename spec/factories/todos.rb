d1e266180190adec37edf634dfe8de1e8353c2a3
MalcyL <malcolm@landonsonline.me.uk>
2020-01-05 16:32:41 +0000

    Update depricated Faker API

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -2,6 +2,6 @@
 FactoryBot.define do
   factory :todo do
     title { Faker::Lorem.word }
-    created_by { Faker::Number.number(10) }
+    created_by { Faker::Number.number(digits: 10) }
   end
 end
